00883d92eb896a4d0e73ae387d72fe658ad095af
hchoroomi <hchoroomi@gmail.com>
2008-11-09 19:07:45 +0330

    Adding new line after header tags

--- convert.rb
+++ convert.rb
@@ -13,9 +13,9 @@ pages.each do |title, body|
   File.open(File.join(OUT_PATH, title.gsub(/\s/, '')), 'w') do |file|
     body.gsub!(/\{\{\{([^\n]+?)\}\}\}/, '<code>\1</code>')
     body.gsub!(/\{\{\{(.+?)\}\}\}/m, '<pre><code>\1</code></pre>')
-    body.gsub!(/====\s(.+?)\s====/, 'h4. \1')
-    body.gsub!(/===\s(.+?)\s===/, 'h3. \1')
-    body.gsub!(/==\s(.+?)\s==/, 'h2. \1')
+    body.gsub!(/====\s(.+?)\s====/, "h4. #{'\1'} \n")
+    body.gsub!(/===\s(.+?)\s===/, "h3. #{'\1'} \n")
+    body.gsub!(/==\s(.+?)\s==/, "h2. #{'\1'} \n")
     body.gsub!(/=\s(.+?)\s=[\s\n]*/, '')
     body.gsub!(/\[(http[^\s\[\]]+)\s([^\[\]]+)\]/, '"\2":\1')
     body.gsub!(/\[([^\s]+)\s(.+)\]/, '"\2":' + GITHUB_WIKI_URL + '\1')
