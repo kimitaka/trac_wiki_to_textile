f29ae101ffa7845e8984d1c529a3ee889bb79582
ataka <masayuki.ataka@gmail.com>
2009-06-27 07:00:27 +0900

    Change output style from github style to textile style
    Signed-off-by: ataka <masayuki.ataka@gmail.com>

--- convert.rb
+++ convert.rb
@@ -10,19 +10,37 @@ pages = db.execute('select name, text from wiki w2 where version = (select max(v
 
 pages.each do |title, body|
   File.open(File.join(OUT_PATH, title.gsub(/\s/, '')), 'w') do |file|
-    body.gsub!(/\{\{\{([^\n]+?)\}\}\}/, '<code>\1</code>')
-    body.gsub!(/\{\{\{(.+?)\}\}\}/m, '<pre><code>\1</code></pre>')
-    body.gsub!(/====\s(.+?)\s====/, "h4. #{'\1'} \n")
-    body.gsub!(/===\s(.+?)\s===/, "h3. #{'\1'} \n")
-    body.gsub!(/==\s(.+?)\s==/, "h2. #{'\1'} \n")
-    body.gsub!(/=\s(.+?)\s=[\s\n]*/, '')
-    body.gsub!(/\[(http[^\s\[\]]+)\s([^\[\]]+)\]/, '"\2":\1')
-    body.gsub!(/\[([^\s]+)\s(.+)\]/, '[[\1 | \2]]')
-    body.gsub!(/([^"\/\!])(([A-Z][a-z0-9]+){2,})/, '\1[[\2]]')
+    body.gsub!(/\r/, '')
+    body.gsub!(/\{\{\{([^\n]+?)\}\}\}/, '@\1@')
+    body.gsub!(/\{\{\{\n#!([^\n]+?)(.+?)\}\}\}/m, '<pre><code class="\1">\2</code></pre>')
+    body.gsub!(/\{\{\{(.+?)\}\}\}/m, '<pre>\1</pre>')
+# macro
+    body.gsub!(/\[\[BR\]\]/, '')
+    body.gsub!(/\[\[PageOutline.*\]\]/, '{{toc}}')
+    body.gsub!(/\[\[Image\((.+?)\)\]\]/, '!\1!')
+# header
+    body.gsub!(/=====\s(.+?)\s=====/, "h5. #{'\1'} \n\n")
+    body.gsub!(/====\s(.+?)\s====/,   "h4. #{'\1'} \n\n")
+    body.gsub!(/===\s(.+?)\s===/,     "h3. #{'\1'} \n\n")
+    body.gsub!(/==\s(.+?)\s==/,       "h2. #{'\1'} \n\n")
+    body.gsub!(/=\s(.+?)\s=[\s\n]*/,  "h1. #{'\1'} \n\n")
+# table
+    body.gsub!(/||/,  "|")
+# link
+    body.gsub!(/\[(http[^\s\[\]]+)\s([^\[\]]+)\]/, ' "\2":\1' )
+    body.gsub!(/\[([^\s]+)\s(.+)\]/, ' [[\1 | \2]] ')
+    body.gsub!(/([^"\/\!])(([A-Z][a-z0-9]+){2,})/, ' \1[[\2]] ')
     body.gsub!(/\!(([A-Z][a-z0-9]+){2,})/, '\1')
+# text decoration
     body.gsub!(/'''(.+)'''/, '*\1*')
     body.gsub!(/''(.+)''/, '_\1_')
+    body.gsub!(/`(.+)`/, '@\1@')
+# itemize
+    body.gsub!(/^\s\s\s\*/, '***')
+    body.gsub!(/^\s\s\*/, '**')
     body.gsub!(/^\s\*/, '*')
+    body.gsub!(/^\s\s\s\d\./, '###')
+    body.gsub!(/^\s\s\d\./, '##')
     body.gsub!(/^\s\d\./, '#')
     file.write(body)
   end
